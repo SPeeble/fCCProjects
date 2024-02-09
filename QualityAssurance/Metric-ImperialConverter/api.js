ec25a1c7794a86ec014c39c56883a06e21ee841e
SPeeble <155892456+SPeeble@users.noreply.github.com>
2024-02-09 03:28:23 -0500

    Create api.js

---
+++ QualityAssurance/Metric-ImperialConverter/api.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const expect = require('chai').expect;
+const ConvertHandler = require('../controllers/convertHandler.js');
+
+module.exports = function (app) {
+  
+  let convertHandler = new ConvertHandler();
+
+  app.route('/api/convert').get((req, res) => {
+    let input = req.query.input;
+    let inputTest =  convertHandler.validateUnit(input)
+    if (typeof inputTest === 'string') { res.send(inputTest) };
+    let initNum = convertHandler.getNum(input);
+    let initUnit = convertHandler.getUnit(input);
+    let returnNum = convertHandler.convert(initNum, initUnit);
+    let returnUnit = convertHandler.getReturnUnit(initUnit);
+    let string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+    res.json({
+      initNum: initNum,
+      initUnit: initUnit,
+      returnNum: returnNum,
+      returnUnit: returnUnit,
+      string: string
+    })
+  })
+  
+};
